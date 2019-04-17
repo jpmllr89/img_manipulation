d6165307d7c88a1203a2f4cd502238ac95957bc4
jpmllr89 <jpmllr89@gmail.com>
2019-04-17 10:23:34 -0400

    got rid of image assigning and just pasted the image directly.

--- main.py
+++ main.py
@@ -6,14 +6,22 @@ from PIL import Image
 #construct the path that points to the picture you're working with, then opens it.
 file = "mtns.jpg"
 #converting an image to mode "L" will make it grayscale
-with Image.open(file).convert("L") as image:
+image = Image.open(file)
 #trying out how to save a new file.  Always call this on the last product (end)
-	newfile = "mountains.jpg"
-	box = (100,100,400,400)
-	region = image.crop(box)
-	#r,g,b = region.split()
-	#region = Image.merge("RGB", (b,r,g))
-	region.save(newfile)
+newfile = "mountains.jpg"
+box = (100,100,400,400)
+region = image.crop(box)
+region = region.convert("L")
+region.show()
+image.paste(region, box)
+image.show()
+#r,g,b = region.split()
+#region = Image.merge("RGB", (b,r,g))
+
 #Debug area
-	print(image.format, image.size, image.mode)
-	print(file)
+print(image.format, image.size, image.mode)
+print(file)
+try:
+	image.save(newfile)
+except AttributeError:
+	print("Could not save photo")
