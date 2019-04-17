097e77e70e6ebc1692b716b7179eca9bd36b4be5
jpmllr89 <jpmllr89@gmail.com>
2019-04-17 11:01:40 -0400

    I have compartmentalized all of the preceding procedural code into a function, however, when run I get a system error, despite keeping the code merely identical.  Apparently the image size doesn't match? Awaiting response on forums.

--- main.py
+++ main.py
@@ -1,27 +1,38 @@
-from __future__ import print_function
 import os, sys
 from PIL import Image
 
 
-#construct the path that points to the picture you're working with, then opens it.
-file = "mtns.jpg"
-#converting an image to mode "L" will make it grayscale
-image = Image.open(file)
-#trying out how to save a new file.  Always call this on the last product (end)
-newfile = "mountains.jpg"
-box = (100,100,400,400)
-region = image.crop(box)
-region = region.convert("L")
-region.show()
-image.paste(region, box)
-image.show()
-#r,g,b = region.split()
-#region = Image.merge("RGB", (b,r,g))
+# file = "mtns.jpg"
+# newfile = "mountains.jpg"
+# #The following will crop a part of the picture and change it to grayscale and put it back.
+# image = Image.open(file)
+# box = (100,100,400,400)
+# region = image.crop(box)
+# region = region.convert("L")
+# #region.show()
+# image.paste(region, box)
+# image.show()
+# #r,g,b = region.split()
+# #region = Image.merge("RGB", (b,r,g))
 
-#Debug area
-print(image.format, image.size, image.mode)
-print(file)
-try:
-	image.save(newfile)
-except AttributeError:
-	print("Could not save photo")
+# #Debug area
+# print(image.format, image.size, image.mode)
+# print(file)
+# try:
+# 	image.save(newfile)
+# except AttributeError:
+# 	print("Could not save photo")
+
+def crop_paste_rect_grayscale(file, newfile, box):
+	img = Image.open(file)
+	region = img.crop(box)
+	region = region.convert("L")
+	return region.show()
+	# img.paste(region, size)
+	# try:
+	# 	img.save(newfile)
+	# except AttributeError:
+	# 	print("Could not save photo")
+	# img.show()
+
+crop_paste_rect_grayscale("mtns.jpg", "mountains.jpg", (100, 100, 100, 100))
