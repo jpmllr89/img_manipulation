6757c31871d35a9898f764837311e9e5fbda678a
jpmllr89 <jpmllr89@gmail.com>
2019-04-16 20:43:54 -0400

    found way how to convert image to grayscale

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from __future__ import print_function
-import os
+import os, sys
 from PIL import Image
 
 
 #construct the path that points to the picture you're working with, then opens it.
 file = "mtns.jpg"
-image = Image.open(file)
-
-
+#converting an image to mode "L" will make it grayscale
+with Image.open(file).convert("L") as image:
+#trying out how to save a new file.  Always call this on the last product (end)
+	newfile = "mountains.jpg"
+	box = (100,100,400,400)
+	region = image.crop(box)
+	#r,g,b = region.split()
+	#region = Image.merge("RGB", (b,r,g))
+	region.save(newfile)
 #Debug area
-print(image.format, image.size, image.mode)
-print(file)
+	print(image.format, image.size, image.mode)
+	print(file)
